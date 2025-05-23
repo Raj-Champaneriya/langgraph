3eea8bc352f203b693971f7c785d8f94790b2c51
Raj Champion <raj.champaneriya@neudesic.com>
2025-05-23 20:43:49 +0530

    docs: enhance agent10.py with detailed comments and prerequisites for better understanding

--- bots/agent10.py
+++ bots/agent10.py
@@ -1,2 +1,15 @@
-# Agentic RAG
+# Agentic RAG :- [doer-checker] pattern with LLM as Judge
+# Learn from [MS sample](https://github.com/Azure-Samples/azure-search-python-samples/blob/main/Quickstart-Agentic-Retrieval/quickstart-agentic-retrieval.ipynb)
+# One agent per data source and another agent to route to appropriate agent.
+
+# Learn how to do orchestration between multiple lang - graphs
+# do we need some async communication pattern to start?
+
 # Report Generating Agent with chart as output
+# Build new copilot with Agent Feed as Need customization to display report.
+# compose using Aspire
+# build graph DB and cosmos to combine sematic and graph details to improve the report result
+# provide an ability to modify meta data and hints to build graph connections
+
+# Prerequisites
+# 1. Postgres DB with many columns and tables and schemas
